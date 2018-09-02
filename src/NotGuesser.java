806f9f1b8857ff157cce6fdc967dd87def9f3c31
Benjamin Auriemma <benjamin.auriemma@duke.edu>
2018-09-02 01:14:49 -0400

    remove the word NOT from queries in NotGuesser

--- src/NotGuesser.java
+++ src/NotGuesser.java
@@ -4,9 +4,9 @@ public class NotGuesser implements Guesser {
 
 	@Override
 	public Guess makeGuess(Trivia trivia) throws IOException {
-		SearchResponse searchResponse1 = new TextSearcher().search(trivia.getQuestion(), trivia.getAnswer1());
-		SearchResponse searchResponse2 = new TextSearcher().search(trivia.getQuestion(), trivia.getAnswer2());
-		SearchResponse searchResponse3 = new TextSearcher().search(trivia.getQuestion(), trivia.getAnswer3());
+		SearchResponse searchResponse1 = new TextSearcher().search(trivia.getQuestion().replace(" NOT ", " "), trivia.getAnswer1());
+		SearchResponse searchResponse2 = new TextSearcher().search(trivia.getQuestion().replace(" NOT ", " "), trivia.getAnswer2());
+		SearchResponse searchResponse3 = new TextSearcher().search(trivia.getQuestion().replace(" NOT ", " "), trivia.getAnswer3());
 		Double answer1Score = Double.valueOf(searchResponse1.numberOfResults());
 		Double answer2Score = Double.valueOf(searchResponse2.numberOfResults());
 		Double answer3Score = Double.valueOf(searchResponse3.numberOfResults());
